4beaf5af8b7bc827094b04049b8e01d21e9d610d
Projkeen Theone <projkeen@outlook.com>
2024-05-23 19:41:22 +0300

    Interfaces

---
+++ Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Interface;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection;
+
+Transport transport = new Transport();
+
+Console.WriteLine("select the transport: <Car>, <Plane>, <Ship>");
+
+    string input = Console.ReadLine();
+    try
+    {
+        if (input.ToLower() == "car")
+        {
+            Car car= new Car();
+            Console.WriteLine(car.Moving());            
+            Console.WriteLine(car.AnotherService());            
+        }
+        if (input.ToLower() == "plane")
+        {
+            Plane plane = new Plane(); 
+            Console.WriteLine(plane.Moving());
+        }
+        if (input.ToLower() == "ship")
+        {
+            Ship ship = new Ship();
+            Console.WriteLine(ship.Moving());
+        }        
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("unknown transport!");
+        Console.WriteLine($"Непредвиденная ошибка: {ex.Message}");        
+    }
+    transport.ShowInfo();
+
---
+++ Interface/Transport.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Interface
+{
+    internal class Transport
+    {
+        public int Number { get; set; }
+        public string Model { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Have fun!");
+        }
+        
+    }
+
+    internal class Plane : Transport, MainFunctions
+    {        
+        public string Moving()
+        {
+            return "This plane is flying";
+        }
+    }
+
+    internal class Car : Transport, MainFunctions, SecondFunctions
+    {
+        public string AnotherService()
+        {
+            return "You may choose a time of the ride";
+        }
+
+        public string Moving()
+        {
+            return "This car is moving";
+        }
+    }
+    internal class Ship : Transport, MainFunctions
+    {
+        public string Moving()
+        {
+            return "This ship is sailing";
+        }
+    }
+}
