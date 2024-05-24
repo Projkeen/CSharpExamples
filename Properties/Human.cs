e1a709e1d8e875b8bc62a0de233e4588336e35dc
Projkeen Theone <projkeen@outlook.com>
2024-05-24 17:43:54 +0300

    Properties

---
+++ Properties/Human.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Human
+    {
+        private string _name;
+        public void SetName(string n)
+        {
+            if (n.Length >= 2)
+                //Console.WriteLine("name too short!");
+                _name = n;
+            else _name = "incorrect name";
+        }
+
+        public string GetName()
+        {
+            //Console.WriteLine(_name);
+            return _name;
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0) 
+                //Console.WriteLine("error!");
+                _age = value;
+            }
+        }
+
+        public void ShowInfo()
+        {  
+            if (_age > 0 && _name.Length >= 2)
+                Console.WriteLine($"Your name is: {_name}, Age: {_age} years old");
+            else Console.WriteLine("Name to short or incorrect age!!!");
+        }
+
+        //public Human(string name, int age)
+        //{
+        //    Name= name;
+        //    Age = age;
+        //}
+
+        //public Human() 
+        //{
+
+        //}
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Properties;
+
+Human human = new Human();
+//{
+//    Name = "AL",
+//    Age = 32
+//};
+Console.WriteLine("What is your name?");
+string n = Console.ReadLine();
+human.SetName(n);
+Console.WriteLine("How old are you?");
+human.Age = Convert.ToInt32(Console.ReadLine());
+human.ShowInfo();
+
+//Random random = new Random();
+//double v = random.Next(8,16);
+//Console.WriteLine(v);
+
+
