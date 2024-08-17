0252f0df537efc11eb8ffb43dfa9ab5babcda101
Projkeen Theone <projkeen@outlook.com>
2024-08-17 17:59:42 +0300

    example initializing private fields via methods

---
+++ WashingMachine/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+using System.Xml.Schema;
+using WashingMachine;
+
+Machine washingMachine = new Machine();
+Console.WriteLine("Через какое время начать стирку? 0 - без отложенной стирки");
+try
+{    
+    washingMachine.TimeBeforeStart = Convert.ToInt32(Console.ReadLine());
+}
+catch(Exception)
+{
+    Console.WriteLine("Введите время в минутах!");    
+}
+
+Console.WriteLine("Выберите режим стирки: <Simple mode>, <Fast MOde>");
+try
+{
+    string mode = Console.ReadLine();
+    if (mode.ToLower()=="simple mode")
+    {
+        washingMachine.SimpleMode();
+    }
+    if (mode.ToLower()=="fast mode")
+    {
+        washingMachine.FastMode();
+    }
+}
+catch(Exception)
+{
+    Console.WriteLine("Введите режим!");
+}
---
+++ WashingMachine/WashingMachine.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WashingMachine
+{
+    internal class Machine
+    {
+        private int rpm;
+        private int washingTime;
+        private string mode;
+        private int timeBeforeStart;
+        private int temperature;
+
+        public int TimeBeforeStart
+        {
+            get { return timeBeforeStart; }
+            set { if (value >= 0) timeBeforeStart = value; 
+            else Console.WriteLine("Без отложеной стирки");
+            }
+        }
+
+        public void SimpleMode()
+        {
+            temperature = 60;
+            rpm = 1000;
+            washingTime = 60;
+            mode = "обычная стирка";
+            if (timeBeforeStart > 0)
+            {
+                Console.WriteLine($"Стирка запустится через {timeBeforeStart} минут(ы).  Режим - <{mode}>. Время стирки составляет {washingTime} минут, температура {temperature} градусов, обороты режима отжима {rpm} оборотов в минуту");
+            }
+            else
+            {
+                Console.WriteLine($"Стирка запущена в режиме <{mode}>. Время стирки составляет {washingTime} минут, температура {temperature} градусов, обороты режима отжима {rpm} оборотов в минуту");
+            }
+        }
+
+        public void FastMode()
+        {
+            temperature = 40;
+            rpm = 1200;
+            washingTime = 15;
+            mode = "быстрая стирка";
+            if (timeBeforeStart > 0)
+            {
+                Console.WriteLine($"Стирка запустится через {timeBeforeStart} минут(ы).  Режим - <{mode}>. Время стирки составляет {washingTime} минут, температура {temperature} градусов, обороты режима отжима {rpm} оборотов в минуту");
+            }
+            else
+            {
+                Console.WriteLine($"Стирка запущена в режиме <{mode}>. Время стирки составляет {washingTime} минут, температура {temperature} градусов, обороты режима отжима {rpm} оборотов в минуту");
+            }
+        }
+
+    }
+}
