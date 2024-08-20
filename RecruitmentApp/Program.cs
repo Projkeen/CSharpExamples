d775c01458b502a285c87bceb4ae45adfd6f3922
Projkeen Theone <projkeen@outlook.com>
2024-08-20 17:44:16 +0300

    initializing filds via constructor, service with methods for class, change fields values

---
+++ RecruitmentApp/Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RecruitmentApp;
+using System.Threading.Channels;
+//Console.WriteLine("Hello, World!");
+while (true)
+{
+    List<Record> records = new List<Record>();
+
+    Record softWareEngineer = new Record(1, "Software Engineer", "Middle", "Created");
+    Record frontEndEngineer = new Record(2, "Frontend Engineer", "Senior", "Created");
+    Record projectManager = new Record(3, "Project Manager", "Trainee", "Created");
+    Record AQA = new Record(4, "AQA", "Junior", "Created");
+    records.Add(softWareEngineer);
+    records.Add(frontEndEngineer);
+    records.Add(projectManager);
+    records.Add(AQA);
+    Service service = new Service();
+    
+    Console.WriteLine("Hello!");
+    while (true)
+    {
+        Console.WriteLine("Vacancy list:");
+        foreach (var record in records)
+        {
+            record.ViewRecord(record);            
+        }
+        Console.WriteLine("Status:");
+        foreach (var record in records)
+        {
+            service.ShowStatus(record);
+        }
+
+        Console.WriteLine("Select operation");
+        Console.WriteLine("1. Sent to review");
+        Console.WriteLine("2. Aprove");
+        Console.WriteLine("3. Reject");
+        Console.WriteLine("4. Exit");
+        Console.WriteLine("_____________________________________________________");
+
+        var input = Console.ReadLine();
+        try
+        {
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("Enter vacancy id for sending to review");
+                    var idToSubmitForApproval = Convert.ToInt32(Console.ReadLine());
+                    foreach (var record in records)
+                    {
+                        if (idToSubmitForApproval == record.Id)
+                        {
+                            service.SubmitForApproval(record);
+                            service.ShowStatus(record);
+                        }
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("Enter vacancy id for approving");
+                    var idToApprove = Convert.ToInt32(Console.ReadLine());
+                    foreach (var record in records)
+                    {
+                        if (idToApprove == record.Id)
+                        {
+                            service.Approve(record);
+                            service.ShowStatus(record);
+                        }
+                    }
+                    break;
+                case "3":
+                    Console.WriteLine("Enter vacancy id for rejecting");
+                    var idToReject = Convert.ToInt32(Console.ReadLine());
+                    foreach (var record in records)
+                    {
+                        if (idToReject == record.Id)
+                        {
+                            service.Reject(record);
+                            service.ShowStatus(record);
+                        }
+                    }
+                    break;
+                case "4":
+                    Console.WriteLine("Good luck");
+                    return;
+                default:
+                    Console.WriteLine("Unknown command");
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+
+        }
+
+
+    }
+}
+
+
+
+
---
+++ RecruitmentApp/Record.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentApp
+{
+    public class Record
+    {
+        public int Id { get; private set; }
+        public string Type { get; private set; }
+        public string Name { get; private set; }
+        private string status;
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public Record(int id, string name, string type, string status)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            Status = status;
+        }
+        public void ViewRecord(Record record)
+        {
+            switch (Type)
+            {
+                case "Middle":
+                    Console.WriteLine($"Vacancy: Id - {Id}. {Name},Type: {Type}, Status: {Status}");
+                    break;                
+                case "Senior":
+                    Console.WriteLine($"Vacancy: Id - {Id}. {Name},Type: {Type}, Status: {Status}");
+                    break;                
+                case "Trainee":
+                    Console.WriteLine($"Vacancy: Id - {Id}. {Name},Type: {Type}, Status: {Status}");
+                    break;                
+                case "Junior":
+                    Console.WriteLine($"Vacancy: Id - {Id}. {Name},Type: {Type}, Status: {Status}");
+                    break;
+                default:
+                    Console.WriteLine($"Unknown {Id}. {Name},Type: {Type}, Status: {Status}");
+                    break;
+            }
+        }
+    }
+
+    public class Service
+    {
+
+        public void SubmitForApproval(Record record)
+        {
+            record.Status = "Sent for review";
+        }
+
+        public void Approve(Record record)
+        {
+            if (record.Status == "Sent for review")
+            {
+                record.Status = "Approved";
+            }
+            throw new InvalidOperationException("You cannot approve record that has not been sending for review");
+        }
+
+        public void Reject(Record record)
+        {
+            if (record.Status == "Sent for review")
+            {
+                record.Status = "Rejected";
+            }
+            throw new InvalidOperationException("You cannot reject record that has not been sending for review");
+        }
+
+        public void ShowStatus(Record record)
+        {
+            Console.WriteLine($"Vacancy {record.Name} - {record.Status}");
+        }
+    }
+}
