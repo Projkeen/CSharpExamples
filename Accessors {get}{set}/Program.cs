44a807c0674281821d6c08962d00ee3a35923a8d
Projkeen Theone <projkeen@outlook.com>
2024-09-08 09:07:04 +0300

    get set

---
+++ Accessors {get}{set}/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Accessors__get__set_;
+
+Wallet wallet=new Wallet();
+//wallet.SetCash(-100);
+wallet.Cash = 100;
+Console.WriteLine(wallet.Cash);
+//wallet.GetCash();
+
---
+++ Accessors {get}{set}/Wallet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accessors__get__set_
+{
+    public class Wallet
+    {
+        private int cash;
+
+        //public Wallet(int cash)
+        //{
+        //       this.cash = cash;
+        //}
+
+        public Wallet()
+        {
+                
+        }
+
+        //public int GetCash()
+        //{
+        //    Console.WriteLine(cash);
+        //    return cash;
+        //}
+
+        //public void SetCash(int value)
+        //{
+        //    if (value >= 0)
+        //        cash = value;
+        //}
+
+        public int Cash
+        {
+            get { return cash; }
+            set { if (value>=0)
+                    cash = value;
+            }
+        }
+    }
+}
