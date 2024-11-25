f7053a099e1c8bab167a2cdb0f457b46d4c9144d
Projkeen Theone <projkeen@outlook.com>
2024-11-25 20:26:07 +0300

    Calculator via OOP

---
+++ CalculatorViaOOP/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorViaOOP
+{
+    internal class Calculator
+    {
+        public double numberOne { get; set; }
+        public double numberTwo { get; set; }
+
+        public double Plus()
+        {
+            return numberOne + numberTwo;
+        }
+
+        public double Minus()
+        {
+            return numberOne - numberTwo;
+        }
+
+        public double Pow()
+        {
+            return numberOne * numberTwo;
+        }
+
+        public double  Divide()
+        {         
+           return numberOne / numberTwo;
+        }
+    }
+}
---
+++ CalculatorViaOOP/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace CalculatorViaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            char operation;
+            double result = 0;
+            bool exit = false;
+            while (!exit)
+            {
+                try
+                {
+                    Console.WriteLine("Enter first number: ");
+                    calc.numberOne = Convert.ToDouble(Console.ReadLine());
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Enter a number!");
+                    continue;
+                }
+
+                try
+                {
+                    Console.WriteLine("Enter operation: ");
+                    operation = Convert.ToChar(Console.ReadLine());
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Enter '+', '-', '*' or '/'!");
+                    continue;
+                }
+
+                try
+                {
+                    Console.WriteLine("Enter second number: ");
+                    calc.numberTwo = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Enter a number!");
+                    continue;
+                }
+                                
+                switch (operation)
+                {
+                    case '+':
+                        result = calc.Plus();
+                        Console.WriteLine("RESULT: " + result);
+                        break;
+                    case '-':
+                        result = calc.Minus();
+                        Console.WriteLine("RESULT: " + result);
+                        break;
+                    case '*':
+                        result = calc.Pow();
+                        Console.WriteLine("RESULT: " + result);
+                        break;
+                    case '/':
+                        if (calc.numberTwo == 0)
+                        {
+                            Console.WriteLine("Error!");
+                            break;
+                        }
+                        result = calc.Divide();
+                        Console.WriteLine("RESULT: " + result);
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }                
+
+                Console.WriteLine("For exit type: <exit> or press <Enter> to continue");
+
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                {
+                    exit = true;
+                    Console.WriteLine("Good luck!");
+                }
+            }      
+        }
+    }
+}
