32e312eda3117e8b176d44f6b1b59fc3a538c08f
Projkeen Theone <projkeen@outlook.com>
2024-11-30 12:16:35 +0300

    Mothod Degree

---
+++ Degree/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Degree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter degree:");
+            int degree = Convert.ToInt32(Console.ReadLine());
+            double result = Degree(number, degree);
+            Console.WriteLine($"Number {number} in degree {degree} equals " + result);
+        }
+        static double Degree(double num, int degr)
+        {
+            double result = 1;
+            for (int i = 0; i < degr; i++)
+            {
+                result = result * num;
+            }
+            return result;
+        }
+    }
+}
